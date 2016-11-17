1f20b986dadeacaa837762f181291739444829fc
Ben Bjurstrom <ben@jelled.com>
2016-11-17 07:42:02 -0800

    Setup symfony console

---
+++ src/DidbotCmd/Commands/ConfigCommand.php
@@ -0,0 +1,22 @@
+<?php
+namespace DidbotCmd\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+
+class ConfigCommand extends Command
+{
+    protected function configure()
+    {
+        //
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        //
+    }
+}
